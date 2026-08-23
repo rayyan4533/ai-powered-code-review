48f1d300dae832e728ee675b4306ae7da2dfc058
Mohammed Rayyan Ansari <mohammedrayyan.ansari280@nmims.edu.in>
2026-08-23 22:03:08 +0530

    app created

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "./generated/prisma/client";
 import { prisma } from "./db";
 import { nextCookies } from "better-auth/next-js";
+
 // If your Prisma file is located elsewhere, you can change the path
 
 
@@ -15,6 +15,11 @@ export const auth = betterAuth({
         github: {
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+
+            mapProfileToUser: async (profile) => ({
+                email: profile.email ?? `${profile.id}@users.noreply.github.com`,
+                name: profile.name ?? profile.login,
+            })
         },
     },
     plugins: [nextCookies()]
