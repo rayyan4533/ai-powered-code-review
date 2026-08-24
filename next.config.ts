2b5967654ec6a7de0fbf7325f27f1665eedcc744
Mohammed Rayyan Ansari <mohammedrayyan.ansari280@nmims.edu.in>
2026-08-24 22:09:15 +0530

    feat: implement GitHub webhook handler with signature verification and Inngest integration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  allowedDevOrigins: ['boneless-dweeb-rewind.ngrok-free.dev'],
+
 };
 
 export default nextConfig;
